fa0a2dd0a644a269f75a1db83b274100b3e3d478
TomassLoiko <box01@inbox.lv>
2021-10-11 23:39:11 +0300

    [feature/practice]: Tamagotchi new add

--- src/main/java/practice/homework/AnimalObjects.java
+++ src/main/java/practice/homework/AnimalObjects.java
@@ -10,7 +10,21 @@ public class AnimalObjects {
         Animal horse = new Animal("Horse", "Smokey",false, 4);
         Animal pig = new Animal("Pig", "Pinki", false, 4);
 
-//        Tamagotchi mouse = new Tamagotchi("Tiger", true, 4);
+        Tamagotchi mouse = new Tamagotchi("Tiger", true, 4);
+
+        mouse.letsDoIt("walk");
+        mouse.letsDoIt("train");
+        mouse.letsDoIt("train");
+        mouse.letsDoIt("play");
+        mouse.letsDoIt("sdsss");
+        mouse.letsDoIt("play");
+        mouse.letsDoIt("eat");
+        mouse.letsDoIt("play");
+        mouse.letsDoIt("dssdsy");
+        mouse.letsDoIt("walk");
+        mouse.letsDoIt("play");
+        mouse.letsDoIt("walk");
+        mouse.letsDoIt("play");
 
 
 
@@ -21,56 +35,59 @@ public class AnimalObjects {
 
 
 
-        tiger.goToSleep();
-        tiger.letsGoWalk();
-        tiger.letsGoPlay();
-        tiger.letsGoExercise();
-        tiger.letsGoWalk();
-        tiger.letsGoExercise();
-        tiger.letsGoEat();
-        tiger.letsGoWalk();
-        tiger.letsGoPlay();
-        tiger.letsGoExercise();
-        tiger.letsGoExercise();
-        tiger.letsGoPlay();
 
-        dog.goToSleep();
-        dog.letsGoWalk();
-        dog.letsGoPlay();
-        dog.letsGoExercise();
-        dog.letsGoWalk();
-        dog.letsGoExercise();
-        dog.letsGoEat();
-        dog.letsGoWalk();
 
-        cat.letsGoEat();
-        cat.letsGoWalk();
-        cat.letsGoPlay();
-        cat.letsGoExercise();
-        cat.letsGoExercise();
-        cat.letsGoPlay();
 
-        hippo.goToSleep();
-        hippo.letsGoWalk();
-        hippo.letsGoPlay();
-        hippo.letsGoExercise();
-        hippo.letsGoWalk();
-        hippo.letsGoEat();
-
-        horse.letsGoPlay();
-        horse.letsGoExercise();
-        horse.letsGoWalk();
-        horse.letsGoExercise();
-        horse.letsGoEat();
-        horse.letsGoWalk();
-        horse.letsGoPlay();
-        horse.letsGoExercise();
-
-        pig.letsGoWalk();
-        pig.letsGoPlay();
-        pig.letsGoExercise();
-        pig.letsGoExercise();
-        pig.letsGoPlay();
+//        tiger.goToSleep();
+//        tiger.letsGoWalk();
+//        tiger.letsGoPlay();
+//        tiger.letsGoExercise();
+//        tiger.letsGoWalk();
+//        tiger.letsGoExercise();
+//        tiger.letsGoEat();
+//        tiger.letsGoWalk();
+//        tiger.letsGoPlay();
+//        tiger.letsGoExercise();
+//        tiger.letsGoExercise();
+//        tiger.letsGoPlay();
+//
+//        dog.goToSleep();
+//        dog.letsGoWalk();
+//        dog.letsGoPlay();
+//        dog.letsGoExercise();
+//        dog.letsGoWalk();
+//        dog.letsGoExercise();
+//        dog.letsGoEat();
+//        dog.letsGoWalk();
+//
+//        cat.letsGoEat();
+//        cat.letsGoWalk();
+//        cat.letsGoPlay();
+//        cat.letsGoExercise();
+//        cat.letsGoExercise();
+//        cat.letsGoPlay();
+//
+//        hippo.goToSleep();
+//        hippo.letsGoWalk();
+//        hippo.letsGoPlay();
+//        hippo.letsGoExercise();
+//        hippo.letsGoWalk();
+//        hippo.letsGoEat();
+//
+//        horse.letsGoPlay();
+//        horse.letsGoExercise();
+//        horse.letsGoWalk();
+//        horse.letsGoExercise();
+//        horse.letsGoEat();
+//        horse.letsGoWalk();
+//        horse.letsGoPlay();
+//        horse.letsGoExercise();
+//
+//        pig.letsGoWalk();
+//        pig.letsGoPlay();
+//        pig.letsGoExercise();
+//        pig.letsGoExercise();
+//        pig.letsGoPlay();
 
 
     }
--- src/main/java/practice/homework/Tamagotchi.java
+++ src/main/java/practice/homework/Tamagotchi.java
@@ -1,74 +1,63 @@
-//package practice.homework;
-//
-//import lombok.ToString;
-//import lombok.extern.slf4j.Slf4j;
-//
-//@ToString
-//@Slf4j
-//
-//
-//
-//public class Tamagotchi {
-//    private String animalName;
-//    private String nickname;
-//    private String command;
-//    private boolean isWild;
-//    private int animalWeight;
-//    private int animalAge;
-//    private int energyLevel;
-//
-//    public int commandForTamagotchi (String command) {
-//        this.command = command;
-//        switch (command) {
-//            case "walk":
-//                energyLevel = 2;
-//                return  energyLevel;
-//                break;
-//            case "eat":
-//                energyLevel = 4;
-//                return  energyLevel;
-//                break;
-//            default:
-//                energyLevel--;
-//                return energyLevel;
-//
-//        }
-//    }
-//
-//        public void letsDoIt () {
-//        String setCommand = "";
-//            if (setCommand != command) {
-//                if (energyLevel > 0) {
-//                    setCommand = command;
-//                    //--energyLevel;
-//                    if (isWild || nickname == null) {
-//                        log.info("The {} now is {}ing", animalName, setCommand);
-//                    } else log.info("{} now is {}ing", nickname, setCommand);
-//                }
-//                if (energyLevel == 0) {
-//                    if (isWild || nickname == null) {
-//                        log.info("The energy level now is {}, please feed the {}", energyLevel, animalName);
-//                    } else log.info("The energy level now is {}, please feed {}", energyLevel, nickname);
-//                }
-//            } else
-//            if (isWild || nickname == null) {
-//                log.info("The {} already {}ing, please specify another command for the {}", animalName, setCommand, animalName);
-//            } else
-//                log.info("{} already {}ing, please specify another command for {}", nickname, setCommand, nickname);
-//        }
-//
-//
-//
-//    public Tamagotchi(String animalName, boolean isWild, int energyLevel) {
-//        this.animalName = animalName;
-//        this.isWild = isWild;
-//        this.energyLevel = energyLevel;
-//    }
-//
-//    public Tamagotchi(String animalName, String nickname, boolean isWild, int energyLevel) {
-//        this.animalName = animalName;
-//        this.nickname = nickname;
-//        this.isWild = isWild;
-//        this.energyLevel = energyLevel;
-//    }
-//}
+package practice.homework;
+
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Objects;
+
+@ToString
+@Slf4j
+
+
+
+public class Tamagotchi {
+    private String animalName;
+    private String nickname;
+    private String command;
+    private String setCommand;
+    private boolean isWild;
+    private int energyLevel;
+
+
+
+        public void letsDoIt (String command) {
+            if (!Objects.equals(command, setCommand)) {
+                setCommand = command;
+                if (Objects.equals(command, "eat")) {
+                    energyLevel = 4;
+                }
+                if (energyLevel > 0) {
+                    if (Objects.equals(command, "walk") || Objects.equals(command, "play") || Objects.equals(command, "train")) {
+                        energyLevel--;
+                    }
+                    if (isWild || nickname == null) {
+                        log.info("The {} now is {}ing", animalName, command);
+                    } else log.info("{} now is {}ing", nickname, command);
+                }
+                if (energyLevel == 0) {
+                    if (isWild || nickname == null) {
+                        log.info("The energy level now is {}, please feed the {}", energyLevel, animalName);
+                    } else log.info("The energy level now is {}, please feed {}", energyLevel, nickname);
+                }
+            } else
+            if (isWild || nickname == null) {
+                log.info("The {} already {}ing, please specify another command for the {}", animalName, command, animalName);
+            } else
+                log.info("{} already {}ing, please specify another command for {}", nickname, command, nickname);
+        }
+
+
+
+    public Tamagotchi(String animalName, boolean isWild, int energyLevel) {
+        this.animalName = animalName;
+        this.isWild = isWild;
+        this.energyLevel = energyLevel;
+    }
+
+    public Tamagotchi(String animalName, String nickname, boolean isWild, int energyLevel) {
+        this.animalName = animalName;
+        this.nickname = nickname;
+        this.isWild = isWild;
+        this.energyLevel = energyLevel;
+    }
+}
